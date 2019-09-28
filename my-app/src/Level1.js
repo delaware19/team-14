8cbc0964bd4eb4f3a282d6f44c00d26b28aa5e0a
meerapatelrox <47922807+meerapatelrox@users.noreply.github.com>
2019-09-28 03:58:53 -0400

    Style changes
    Level 1 page improvements, moving on to back-end

--- my-app/src/Level1.js
+++ my-app/src/Level1.js
@@ -20,26 +20,26 @@ function Level1() {
 
         <p>
 
-        <label for="First-Name-4" class="apply-field-label-large first">In this page, you're able to leave your information and upload any relavent documents you may have. After submittin, you're able to proceed to the next phase.</label>
+        <label for="First-Name-4" class="apply-field-label-large first">Level 4: Meet with like-minded
+        groups (nonprofits, cultural influences, etc.) and share your vision for change. Goal: make one
+        contact and upload their information below. Reach out! After submitting, you're may proceed to 
+        the next phase.</label> <br></br>
 
-        <label class="apply-field-label-medium">Name *</label><div class="apply-form-group"><div class="apply-form-group-item w-clearfix"><input type="text" class="apply-text-field w-input" maxlength="256" name="Full-Name-4" data-name="Full Name 4" id="Full-Name-4" required=""/></div>
-
-        </div><div class="apply-form-group-item half"><label for="Phone" class="apply-field-label-medium">Phone *</label><input type="tel" class="apply-text-field w-input" maxlength="256" name="Phone" data-name="Phone" id="Phone" required=""/></div><div class="apply-form-group-item half"><label for="Email" class="apply-field-label-medium">Email *</label><input type="email" class="apply-text-field w-input" maxlength="256" name="Email" data-name="Email" id="Email" required=""/><label class="apply-form-labels">Upload File</label><input type="text" class="apply-text-field w-input" maxlength="256" name="upload file" data-name="upload file" id="upload-file-1" required=""/></div>
-
+        <label class="apply-field-label-medium">Name *</label><div class="apply-form-group"><div class="apply-form-group-item w-clearfix"><input type="text" class="apply-text-field w-input" maxlength="256" name="Full-Name-4" data-name="Full Name 4" id="Full-Name-4" required=""/></div></div>
+        
+        <br></br>
+        <div class="apply-form-group-item half"><label for="Phone" class="apply-field-label-medium">Phone *</label><input type="tel" class="apply-text-field w-input" maxlength="256" name="Phone" data-name="Phone" id="Phone" required=""/></div>
+        
+        <br></br>
+        <div class="apply-form-group-item half"><label for="Email" class="apply-field-label-medium">Email *</label><input type="email" class="apply-text-field w-input" maxlength="256" name="Email" data-name="Email" id="Email" required=""/>
+        
+        <br></br>
+        <br></br>
+        <label class="apply-form-labels">Upload File</label><input type="text" class="apply-text-field w-input" maxlength="256" name="upload file" data-name="upload file" id="upload-file-1" required=""/></div>
+        <br></br>
+        
         <style>
-        .button {
-        border: none;
-        color: black;
-        padding: 20px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        cursor: pointer;
-        }
-
-        .button {border-radius: 12px;}
+        
         </style>
 
 
