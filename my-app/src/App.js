5bbe6aeeb0a738a77a132f6913fa0a7e138d3312
meerapatelrox <47922807+meerapatelrox@users.noreply.github.com>
2019-09-28 02:12:13 -0400

    Stylistic changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -30,10 +30,20 @@ function App() {
         </Route>
 
         <Route path="/">
-          <header className="App-header"></header>
-          <p> Welcome to Rise! <br></br> <br></br>
+          <header className="App-header">
+          <img src="https://uploads-ssl.webflow.com/5c98fd7e7e3bc9bee0816b45/5ca67dd40adf050aede96294_Asset%201.svg"/>  
+          </header>
+          <br></br><h3> Welcome to Rise!</h3>
+          <div class="container"><p> Rise is grounded in the belief that the voices of ordinary citizens matter, 
+            regardless of their background or age. This social movement blueprint revolutionizes 
+            how laws are created by organizing and empowering citizens to pen their own rights 
+            into existence. By identifying key battleground states, catalyst points for the movement, 
+            Rise creates a ripple of inspiration in other states across the nation demanding these 
+            rights are codified. The process—training organizers, telling compelling narratives in 
+            the media,creating visual cultural moments of protest, pushing legislators and building 
+            support between unexpected allies—has helped pass 21 bills to date.</p></div><br></br><br></br>
           <button class="btn"><Link to="/login">Login</Link></button> <br></br> <br></br>
-          <button class="btn"><Link to="/createaccount">Create an Account</Link></button> <br></br></p>
+          <button class="btn"><Link to="/createaccount">Create an Account</Link></button> <br></br>
         </Route>
 
       </Switch>
