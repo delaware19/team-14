d4b9f557738092efbded2e554aed47764cab02d8
meerapatelrox <47922807+meerapatelrox@users.noreply.github.com>
2019-09-28 07:15:06 -0400

    Pushing Login and Settings basic pages

--- my-app/src/Settings.js
+++ my-app/src/Settings.js
@@ -22,10 +22,12 @@ function Settings() {
       </nav>
       </header>
 
-      <img src={logo} className="App-logo" alt="logo" /> <br></br>
-
         <p>
-          This is the Settings page.
+        <h3>SETTINGS</h3>
+
+            <button class="btn"><Link to="/">Edit Username</Link></button> <br></br><br></br>
+        <button class="btn"><Link to="/">Change Email</Link></button> <br></br><br></br>
+        <button class="btn"><Link to="/">Change Password</Link></button> <br></br><br></br>
         </p>
     </div>
   );
