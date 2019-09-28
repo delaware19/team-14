206be46812aa741e4a961e69fea5eba1bbad7756
Liang Fang <32380703+devliang@users.noreply.github.com>
2019-09-28 04:16:23 -0400

    Update "Choose File" button

--- my-app/src/Level1.js
+++ my-app/src/Level1.js
@@ -20,31 +20,38 @@ function Level1() {
 
         <p>
 
-        <label for="First-Name-4" class="apply-field-label-large first">Level 4: Meet with like-minded
-        groups (nonprofits, cultural influences, etc.) and share your vision for change. Goal: make one
-        contact and upload their information below. Reach out! After submitting, you're may proceed to 
-        the next phase.</label> <br></br>
+        <label for="First-Name-4" class="apply-field-label-large first">In this page, you're able to leave your information and upload any relavent documents you may have. After submittin, you're able to proceed to the next phase.</label>
 
-        <label class="apply-field-label-medium">Name *</label><div class="apply-form-group"><div class="apply-form-group-item w-clearfix"><input type="text" class="apply-text-field w-input" maxlength="256" name="Full-Name-4" data-name="Full Name 4" id="Full-Name-4" required=""/></div></div>
-        
-        <br></br>
+        <div class="apply-form-group-item half"><label for="Name" class="apply-field-label-medium">Name *</label><input type="text" class="apply-text-field w-input" maxlength="256" name="Text" data-name="Text" id="Text" required=""/></div>
+
         <div class="apply-form-group-item half"><label for="Phone" class="apply-field-label-medium">Phone *</label><input type="tel" class="apply-text-field w-input" maxlength="256" name="Phone" data-name="Phone" id="Phone" required=""/></div>
-        
-        <br></br>
+
         <div class="apply-form-group-item half"><label for="Email" class="apply-field-label-medium">Email *</label><input type="email" class="apply-text-field w-input" maxlength="256" name="Email" data-name="Email" id="Email" required=""/>
-        
-        <br></br>
-        <br></br>
-        <label class="apply-form-labels">Upload File</label><input type="text" class="apply-text-field w-input" maxlength="256" name="upload file" data-name="upload file" id="upload-file-1" required=""/></div>
-        <br></br>
-        
+
+        <form action="/action_page.php">
+        <input type="file" name="pic" accept="image/*">
+        </form>
+
         <style>
-        
-        </style>
+      .button {
+        border: none;
+        color: black;
+        padding: 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+      }
+
+      .button {border-radius: 12px;}
+      </style>
 
 
-        <button type="button" onclick="alert('Submitted')" button class="button button">Submit</button>
-        <button type="button" onclick="alert('Saved')" button class="button button">Save For Later</button>
+      <button type="button" onclick="alert('Submitted')" button class="button button">Submit</button>
+      <button type="button" onclick="alert('Saved')" button class="button button">Save For Later</button>
+
 
         </p>
     </div>
